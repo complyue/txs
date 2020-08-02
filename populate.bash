7ce7bec838f9ac1988fbdfcfda0c778a358ec590
Compl Yue <compl.yue@icloud.com>
2020-08-02 17:48:52 +0800

    all intermediate file go into build/

--- populate.bash
+++ populate.bash
@@ -4,8 +4,8 @@ cd "$(dirname "$0")"
 mkdir build 2>/dev/null
 cd build
 
-ghc --make -Wall -threaded -rtsopts -prof -o txs -i../src ../src/Main.hs && (
+ghc --make -Wall -threaded -rtsopts -prof -o txs -outputdir . -stubdir . -i../src ../src/Main.hs && (
 
-  ./txs +RTS -N6 -A32m -H256m -qg -I0 -M2g -T -s < ../scripts/populate.txs
+  ./txs +RTS -N6 -A32m -H256m -qg -I0 -M2g -T -s <../scripts/populate.txs
 
 )
