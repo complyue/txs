1d69b54ed8d3962d7a34e930a43a2b7a0bf34c81
ComplYue <compl.yue@icloud.com>
2020-09-14 23:02:18 +0800

    play with impl. of named

--- poc/apk.hs
+++ poc/apk.hs
@@ -1,10 +1,17 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedLabels, DataKinds, TypeOperators, ViewPatterns,
+             PartialTypeSignatures #-}
 
 module Main where
 
 import           Prelude
-import           GHC.Generics
+import           Data.Functor.Identity          ( Identity(..) )
+import           GHC.OverloadedLabels           ( IsLabel(..) )
+
 import           Data.Dynamic
+
+import           Named.Internal1
 
 
 -- * minimum data structures as interface with scripting code
@@ -34,76 +41,52 @@ class Callable a where
 
 -- * functions to be callable from scripting code
 
-newtype Assert = Assert (
-    Expect -> Maybe Target -> Message -> IO Message
-  )
-type Expect = AttrVal
-type Target = AttrVal
-type Message = String
-
-instance Callable Assert where
-
-  -- can this get auto-generated ? with https://wiki.haskell.org/GHC.Generics
-  call (Assert !assert) (ArgsPack !args !kwargs) !exit = do
-    (expect, target, message) <- parseApk
-    result                    <- assert expect target message
-    exit $ StrValue result
-   where
-
-    parseApk :: IO (Expect, Maybe Target, Message)
-    parseApk = goParse
-      (Left "missing arg: expect", Nothing, Left "missing arg: message")
-      args
-      kwargs
-
-    goParse (got'expect, got'target, got'message) [] [] = case got'expect of
-      Left  msg    -> error msg
-      Right expect -> case got'message of
-        Left  msg     -> error msg
-        Right message -> return (expect, got'target, message)
-    goParse (got'expect, got'target, got'message) args' ((name, val) : kwargs')
-      = case name of
-        "expect" -> case got'expect of
-          Right{} -> error "duplicate arg: expect"
-          Left{}  -> goParse (Right val, got'target, got'message) args' kwargs'
-        "target" -> case got'target of
-          Just{}  -> error "duplicate arg: target"
-          Nothing -> goParse (got'expect, Just val, got'message) args' kwargs'
-        "message" -> case got'message of
-          Right{} -> error "duplicate arg: message"
-          Left{}  -> case val of
-            StrValue message ->
-              goParse (got'expect, got'target, Right message) args' kwargs'
-            _ -> error "bad arg type for: message"
-        _ -> error "unexpected keyword args"
-    goParse (got'expect, got'target, got'message) (val : args') [] =
-      case got'expect of
-        Left{}  -> goParse (Right val, got'target, got'message) args' []
-        Right{} -> case got'target of
-          Nothing -> goParse (got'expect, Just val, got'message) args' []
-          Just{}  -> case got'message of
-            Left{} -> case val of
-              StrValue message ->
-                goParse (got'expect, got'target, Right message) args' []
-              _ -> error "bad arg type for: message"
-            Right{} -> error "extranous positional args"
-
-
--- mockup & test out
-main :: IO ()
-main =
-  call
-      (Assert assert)
-      (ArgsPack [IntValue 333, StrValue "as good will"]
-                [("target", IntValue 333)]
-      )
-    $ \result -> putStrLn $ "Got result: " <> show result
-
--- | plain Haskell function meant to be easily called by scripting code
-assert :: Expect -> Maybe Target -> Message -> IO Message
-assert !expect !maybeTarget !message = case maybeTarget of
+-- | interfacing Haskell function meant to be easily called by scripting code
+assert
+  :: "expect" :! AttrVal
+  -> "target" :? AttrVal
+  -> "message" :! String
+  -> IO String
+assert (Arg !expect) (ArgF !maybeTarget) (Arg !message) = case maybeTarget of
   Nothing     -> return $ "* assertion not applicable: " <> message
   Just target -> if expect == target
     then return $ "* assertion passed: " <> message
     else error $ "* assertion failed: " <> message
 
+
+-- mockup & test out
+main :: IO ()
+main = do
+
+  result <- assert3 ! defaults
+
+  putStrLn $ "Got result: " <> show result
+
+ where
+
+  assert1' = with
+    (Param (ArgF @Identity @String @"message" (Identity "as good will")))
+    assert
+
+  messageValue = "as good will"
+  assert1''    = with (fromLabel @"message" messageValue) assert
+
+  assert1'''   = with (#message "as good will") assert
+
+  assert1 =
+    assert --
+           ! #message "as good will"
+
+  assert2 =
+    assert --
+      ! #message "as good will"
+      ! #expect (IntValue 333)
+
+  assert3 =
+    assert -- 
+      ! #message "as good will"
+      ! #expect (IntValue 333)
+      ! #target (IntValue 333)
+
+  !apk =
+    ArgsPack [IntValue 333, StrValue "as good will"] [("target", IntValue 333)]
